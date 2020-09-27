3dbfb7c35a93a4186b12bda5cc74c9eb0bec92f4
Timm <timur.galiakberov@simbirsoft.com>
2020-09-27 07:01:15 +0300

    Регистрация. Пока без проверки почты и телефона

--- src/main/java/com/craftsmengroup/paymentsystem/authserver/exception/UserExistException.java
+++ src/main/java/com/craftsmengroup/paymentsystem/authserver/exception/UserExistException.java
@@ -1,7 +1,5 @@
 package com.craftsmengroup.paymentsystem.authserver.exception;
 
-import com.craftsmengroup.paymentsystem.authserver.exception.AuthException;
-
 public class UserExistException extends AuthException {
     public UserExistException(String message) {
         super(message);
